afa20ecbe1bb279c9bb425bd55dc989c772ffd45
pavansuthar567 <119834324+chiragsuthar55@users.noreply.github.com>
2023-01-11 01:10:01 +0530

    keep app alive 8

--- index.js
+++ index.js
@@ -546,6 +546,7 @@ app.get("/", async function (req, res) {
 
 function startKeepAlive() {
   setInterval(function () {
+    console.log("startKeepAlive");
     try {
       var options = {
         host: "instagram-poster.onrender.com",
@@ -557,7 +558,7 @@ function startKeepAlive() {
           res.on("data", function (chunk) {
             try {
               // optional logging... disable after it's working
-              console.log("HEROKU RESPONSE: " + chunk);
+              console.log("HEROKU RESPONSE: 8" + chunk);
             } catch (err) {
               console.log(err.message);
             }
@@ -567,7 +568,7 @@ function startKeepAlive() {
           console.log("Error: " + err.message);
         });
     } catch (error) {
-      console.log("keep alive", error);
+      console.log("keep alive error", error);
     }
   }, 5000); // load every 10 minutes
   // }, 10 * 60 * 1000); // load every 10 minutes
